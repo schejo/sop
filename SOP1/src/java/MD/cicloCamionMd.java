6a673dacb7cf44c19ac7e2a9af761135086f1f32
Sergio Perez <schejo2@gmail.com>
2022-12-30 09:54:12 -0600

    commit correcto de los cambios de jefe

---
+++ SOP1/src/java/MD/cicloCamionMd.java
@@ -0,0 +1,238 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MD;
+
+/**
+ *
+ * @author HP 15
+ */
+public class cicloCamionMd {
+
+    private String fechaCiclo;
+    private String ciclom;
+    
+   
+    private String codPais;
+    private String licencia;
+    private String codDesti;
+    private String seccDesti;
+    private String tipOpera;
+    
+    
+    private String fIngRec;
+    private String anoArribo;
+    private String numArribo;
+   
+    private String numPlaca;
+    private String fAlta;
+    private String indicador;
+   
+    
+   
+    private String tipoVia;
+
+    private String ingCont1;
+    private String ingCont2;
+    private String ingCont3;
+  
+    private String fIniDat;
+    private String fFinDat;
+    
+    //mensajes del sistema
+    private String resp;
+    private String msg;
+    private String dir;
+
+    public String getSeccDesti() {
+        return seccDesti;
+    }
+
+    public void setSeccDesti(String seccDesti) {
+        this.seccDesti = seccDesti;
+    }
+
+    public String getCodPais() {
+        return codPais;
+    }
+
+    public void setCodPais(String codPais) {
+        this.codPais = codPais;
+    }
+
+    public String getLicencia() {
+        return licencia;
+    }
+
+    public void setLicencia(String licencia) {
+        this.licencia = licencia;
+    }
+
+    public String getCodDesti() {
+        return codDesti;
+    }
+
+    public void setCodDesti(String codDesti) {
+        this.codDesti = codDesti;
+    }
+
+    public String getTipOpera() {
+        return tipOpera;
+    }
+
+    public void setTipOpera(String tipOpera) {
+        this.tipOpera = tipOpera;
+    }
+
+  
+
+    public String getfIngRec() {
+        return fIngRec;
+    }
+
+    public void setfIngRec(String fIngRec) {
+        this.fIngRec = fIngRec;
+    }
+
+    public String getAnoArribo() {
+        return anoArribo;
+    }
+
+    public void setAnoArribo(String anoArribo) {
+        this.anoArribo = anoArribo;
+    }
+
+    public String getNumArribo() {
+        return numArribo;
+    }
+
+    public void setNumArribo(String numArribo) {
+        this.numArribo = numArribo;
+    }
+
+
+
+    public String getNumPlaca() {
+        return numPlaca;
+    }
+
+    public void setNumPlaca(String numPlaca) {
+        this.numPlaca = numPlaca;
+    }
+
+    public String getfAlta() {
+        return fAlta;
+    }
+
+    public void setfAlta(String fAlta) {
+        this.fAlta = fAlta;
+    }
+
+    public String getIndicador() {
+        return indicador;
+    }
+
+    public void setIndicador(String indicador) {
+        this.indicador = indicador;
+    }
+
+
+  
+
+    public String getTipoVia() {
+        return tipoVia;
+    }
+
+    public void setTipoVia(String tipoVia) {
+        this.tipoVia = tipoVia;
+    }
+
+ 
+
+    public String getIngCont1() {
+        return ingCont1;
+    }
+
+    public void setIngCont1(String ingCont1) {
+        this.ingCont1 = ingCont1;
+    }
+
+    public String getIngCont2() {
+        return ingCont2;
+    }
+
+    public void setIngCont2(String ingCont2) {
+        this.ingCont2 = ingCont2;
+    }
+
+    public String getIngCont3() {
+        return ingCont3;
+    }
+
+    public void setIngCont3(String ingCont3) {
+        this.ingCont3 = ingCont3;
+    }
+
+   
+
+    public String getfIniDat() {
+        return fIniDat;
+    }
+
+    public void setfIniDat(String fIniDat) {
+        this.fIniDat = fIniDat;
+    }
+
+    public String getfFinDat() {
+        return fFinDat;
+    }
+
+    public void setfFinDat(String fFinDat) {
+        this.fFinDat = fFinDat;
+    }
+    
+    
+
+    public String getFechaCiclo() {
+        return fechaCiclo;
+    }
+
+    public void setFechaCiclo(String fechaCiclo) {
+        this.fechaCiclo = fechaCiclo;
+    }
+
+    public String getCiclom() {
+        return ciclom;
+    }
+
+    public void setCiclom(String ciclom) {
+        this.ciclom = ciclom;
+    }
+
+    public String getResp() {
+        return resp;
+    }
+
+    public void setResp(String resp) {
+        this.resp = resp;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDir() {
+        return dir;
+    }
+
+    public void setDir(String dir) {
+        this.dir = dir;
+    }
+
+}
