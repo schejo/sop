ae5ae2cd9602d39c94142ebadcd26f9540296cc7
Sergio Perez <schejo2@gmail.com>
2023-01-23 16:03:50 -0600

    commit

---
+++ SOP1/src/java/ctrl/VerActividadCtrl.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ctrl;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+
+/**
+ *
+ * @author Usuario
+ */
+public class VerActividadCtrl extends GenericForwardComposer{
+    
+    @Override
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        
+    }
+    
+    
+    
+}
