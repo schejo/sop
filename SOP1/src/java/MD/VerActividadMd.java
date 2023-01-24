50f6a65f81db87c965ca1afdfad24703a10671b9
RamyelR1 <ramiroantoniohernandezx50@gmail.com>
2023-01-24 08:14:21 -0600

    commit
    modal de actividades

---
+++ SOP1/src/java/MD/VerActividadMd.java
@@ -0,0 +1,52 @@
+package MD;
+
+public class VerActividadMd {
+    
+    private String actividad;
+    private String fecha_act;
+    private String hora_act;
+    private String nom_duracion;
+    private String duracion;
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+    
+    public String getFecha_act() {
+        return fecha_act;
+    }
+
+    public void setFecha_act(String fecha_act) {
+        this.fecha_act = fecha_act;
+    }
+
+    public String getHora_act() {
+        return hora_act;
+    }
+
+    public void setHora_act(String hora_act) {
+        this.hora_act = hora_act;
+    }
+
+    public String getNom_duracion() {
+        return nom_duracion;
+    }
+
+    public void setNom_duracion(String nom_duracion) {
+        this.nom_duracion = nom_duracion;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+    
+    
+}
---
+++ SOP1/src/java/ctrl/VerActividadCtrl.java
@@ -0,0 +1,80 @@
+package ctrl;
+
+import DAL.VerActividadDal;
+import MD.CatalogosMd;
+import MD.VerActividadMd;
+import java.util.ArrayList;
+import java.util.List;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Session;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.ListModelList;
+import org.zkoss.zul.Listbox;
+import org.zkoss.zul.Textbox;
+import org.zkoss.zul.Window;
+
+public class VerActividadCtrl extends GenericForwardComposer {
+
+    Window modalDialog;
+    List<VerActividadMd> lista = new ArrayList<VerActividadMd>();
+    List<VerActividadMd> data = new ArrayList<VerActividadMd>();
+    private Listbox lb2;
+    VerActividadDal ctd = new VerActividadDal();
+    Session Session = Sessions.getCurrent();
+    
+    private Textbox nom_actividad;
+    private Textbox fecha_actividad;
+    private Textbox hora_actividad;
+    private Textbox nombre_duracion;
+    private Textbox duracion;
+    
+    public void doAfterCompose(Component comp) throws Exception {
+        super.doAfterCompose(comp);
+        
+        lista = ctd.consulta(Session.getAttribute("anioArribo").toString(), Session.getAttribute("numeroArribo").toString());
+        data.clear();
+
+        for (VerActividadMd item : lista) {
+
+            VerActividadMd da = new VerActividadMd();
+            da.setActividad(item.getActividad());
+            da.setFecha_act(item.getFecha_act());
+            da.setHora_act(item.getHora_act());
+            da.setNom_duracion(item.getNom_duracion());
+            da.setDuracion(item.getDuracion());
+
+            data.add(da);
+
+        }
+        lb2.setModel(new ListModelList(data));
+
+    }
+
+    public void onClick$btnAgregar(Event e) {
+        List<VerActividadMd> items = new ArrayList<VerActividadMd>();
+        VerActividadMd data = new VerActividadMd();
+        data.setActividad(nom_actividad.getText());
+        data.setFecha_act(fecha_actividad.getText());
+        data.setHora_act(hora_actividad.getText());
+        data.setNom_duracion(nombre_duracion.getText());
+        data.setDuracion(duracion.getText());
+
+        items.add(data);
+        EventQueues.lookup("myEventQueue", EventQueues.DESKTOP, true)
+                .publish(new Event("onChangeNickname", null, items));
+        modalDialog.detach();
+    }
+
+    public void onClick$btnSalir(Event e) {
+        List<VerActividadMd> items = new ArrayList<VerActividadMd>();
+         EventQueues.lookup("myEventQueue", EventQueues.DESKTOP, true)
+                .publish(new Event("onChangeNickname", null, items));
+
+        modalDialog.detach();
+        
+    }
+
+}
